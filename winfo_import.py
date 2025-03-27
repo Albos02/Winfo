af385b97ffe741a60758d78cecf6a3cd54518b37
Albos02 <alex@bossel.ch>
2025-03-27 13:11:22 +0100

    logger.info() for winfo_import.py

--- winfo_import.py
+++ winfo_import.py
@@ -5,12 +5,14 @@ from datetime import datetime
 import tzlocal
 from PIL import Image
 from winfo_constants import *
+from winfo_classes import logger
 
 latest_date = None
 
 with open('conversion_key_tkinter_to_keyboard.json', 'r') as file:
     conversion_key_tkinter_keyboard = json.load(file)
 def create_errored_file(error):
+    logger.info('creating_errored_file')
     GMT_datetime = f'{time.gmtime().tm_year}{time.gmtime().tm_mon}{time.gmtime().tm_mday}{time.gmtime().tm_hour}{round(time.gmtime().tm_min, -1)}'
     with open('VQHA80.csv', 'w') as f:
         f.write('Station/Location;Date;tre200s0;rre150z0;sre000z0;gre000z0;ure200s0;tde200s0;dkl010z0;fu3010z0;fu3010z1;prestas0;pp0qffs0;pp0qnhs0;ppz850s0;ppz700s0;dv1towz0;fu3towz0;fu3towz1;ta1tows0;uretows0;tdetows0\n')
@@ -18,16 +20,17 @@ def create_errored_file(error):
             f.write(f'{station[0]};{GMT_datetime};-;-;-;-;-;-;{error};-;-;-;-;-;-;-;-;-;-;-;-;-\n')
 
 def reload_preferences():
+    logger.info('reloading_preferences')
     global preferences, first_boot
     try:
         with open('preferences.json', 'r') as f:
             preferences = json.load(f)
         first_boot = False
     except FileNotFoundError:
-        print('no preferences have been saved yet')
+        logger.info('no preferences have been saved yet')
         first_boot = True
     except:
-        print('error loading preferences')
+        logger('error loading preferences')
         preferences = {}
         first_boot = True
     try:
@@ -58,6 +61,7 @@ def get_text_icon_arrow(direction):
     return output
 
 def alert_content():
+    logger.info('creating alert_content')
     with open("VQHA80.csv", "r") as f_csv:
         reader = csv.DictReader(f_csv, delimiter=';')
         reload_preferences()
@@ -86,6 +90,7 @@ def alert_content():
                     for i in range(2): value.append('')
             return content, date
 def strafe_date_from_csv(local_time: str):
+    logger.info('strafe_date_from_csv -> local_time: ' + local_time)
     gmt_time = datetime.strptime(local_time, '%Y%m%d%H%M')
     gmt_time = pytz.timezone('GMT').localize(gmt_time)
 
@@ -101,6 +106,7 @@ def strafe_date_from_csv(local_time: str):
     date = local_time.strftime('%d/%m/%Y')
     return hour, minute, date
 def send_alert(station, *args):
+    logger.info('sending alert')
     try:
         date = args[0]
         if date != '':
@@ -153,9 +159,10 @@ def send_alert(station, *args):
     elif platform.system().lower() == 'darwin':
         subprocess.run(["osascript", "-e", '\n'.join(text_fields)], check=True)
 
-    print('notification sent to ', station)
+    logger.info('notification sent to ', station)
 
 def launch_winfo():
+    logger.info('launching winfo')
     subprocess.Popen(['winfo.py'])
 def set_icon(moyenne, rafale):
 
@@ -181,7 +188,7 @@ def set_icon(moyenne, rafale):
         force_couleur = int((moyenne-60)/25 * 255)
         pixel_values = (int(255/8-force_couleur/8), int(255/16-force_couleur/16), 0, 255) # to full black
 
-    print(f'{moyenne}\n {force_couleur = } | -> {pixel_values = }')
+    # print(f'{moyenne}\n {force_couleur = } | -> {pixel_values = }')
     im = Image.open('images/wind_arrow.png').convert('RGBA')
     width, height = im.size
     data = list(im.getdata())
@@ -216,20 +223,26 @@ for station in station_dict.keys(): station_list.append(str(station))
 
 reload_preferences()
 
+logger.info('openning preferences.json')
 with open('language_dict.json', 'r') as file:
     language_dict = json.load(file)
     try:
         if preferences['language'] == 'English':
             lang_index = 0
+            logger.info('English language')
         elif preferences['language'] == 'Français':
             lang_index = 1
+            logger.info('Français language')
     except:
         lang_index = 1
 try:
     if preferences['wind_speed_unit'] == 'kph':
         wind_speed_coef = 1
+        logger.info('wind_speed_unit = kph')
     else:
         wind_speed_coef = 1/1.852
+        logger.info('wind_speed_unit = knots')
 except KeyError:
     wind_speed_coef = 1
     preferences['wind_speed_unit'] = 'kph'
+    logger.info('wind_speed_unit = kph')
